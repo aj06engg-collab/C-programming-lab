84cee507bdf325039ca977d3ce1992877dc4a093
Ankit Jana <aj06engg@gmail.com>
2025-10-25 20:43:52 +0530

    Create kinetic_energy.c

---
+++ Lab_2/kinetic_energy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  float Mass,Velocity,KE;
+printf("Kinetic Energy Calculator\n");
+printf("formula; KE = .5 * mass * (velocity)2\n");
+
+pritnf("Enter Mass (kg): ");
+scamnf(%f", &Mass);
+
+  printf("Enter Velocity ( m/s): ");
+  scanf("%F", &Velocity);
+
+KE = .5 * Mass * (Velocity)(Velocity);
+printf("KInetic Energy = %2f Joules\n", KE);
+
+//End of program 
+return 0;
+}

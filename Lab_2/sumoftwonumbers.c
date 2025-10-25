ffa944de59045d022a60de05549dbb1624deecbd
Ankit Jana <aj06engg@gmail.com>
2025-10-25 20:52:49 +0530

    Create sumoftwonumbers.c

---
+++ Lab_2/sumoftwonumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void) {
+float a ,b , sum;
+
+printf("Enter first number: ");
+scanf(%f", &a);
+
+  printf("Enter second numbr: ");
+scanf("%F", &b);
+
+sum = a + b ;
+printf("Sum = %f\n", sum);
+
+return 0;
+}

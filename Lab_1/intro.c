019dfc50e48d9537f45c8be7dcc426a81ef1a0ff
Ankit Jana <aj06engg@gmail.com>
2025-10-25 20:14:24 +0530

    Create intro.c

---
+++ Lab_1/intro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  //printing intro to the screen
+printf("My name is Ankit Jana\n");
+printf(" Roll Number: MEB25058\n");
+pritnf("Department: Mechanucal Engeneering\n");
+printf("University: Tezpur University\n");
+
+//emding the program
+return 0:
+  }
